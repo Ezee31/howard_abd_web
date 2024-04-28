7b364c011a8dd55448338915c39b9e9d16abf8b0
Ezee31 <ezequiel.chavez3103@gmail.com>
2024-04-27 23:54:07 -0600

    se agrego la aplicacion howard web y la plantilla home

--- howard_abd_web/settings.py
+++ howard_abd_web/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # howard apps
+    'howard_web.apps.HowardWebConfig'
+    
 ]
 
 MIDDLEWARE = [
