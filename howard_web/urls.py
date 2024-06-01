cd791e2f53453c6a16ae422b530af2860135f7e7
Ezee31 <ezequiel.chavez3103@gmail.com>
2024-05-31 23:40:37 -0600

    Co-authored-by: Héctor Montoya <hecmondev@users.noreply.github.com>

--- howard_web/urls.py
+++ howard_web/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("", views.home, name="home"),
-  path("login", views.login, name="login"),
+  path("", views.signin, name="signin"),
+  #path("login", views.login, name="login"),
   path("dashboard", views.dashboard, name="dashboard")
 ]
--- howard_web/views.py
+++ howard_web/views.py
@@ -1,13 +1,35 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here. 
 
 def home(request): 
     return render(request,"home.html")
 
-def login(request):
-    return render(request,"login/index.html")
+#Login
+#@login_required(login_url='login')
+def signin(request):
+    if request.method == "GET":
+        return render(request,"login/index.html")
+    if request.method == "POST":
+        login_form = LoginForm(request.POST)
+        is_valid = login_form.is_valid()
+        if is_valid:
+            username = login_form.cleaned_data["username"]
+            password = login_form.cleaned_data["password"]
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect("dashboard")
+        else:
+            new_login_form = LoginForm()
+            return render(request, "login/index.html", {'form': new_login_form})
 
 def dashboard(request):
     return render(request,"dashboard/index.html")
+
+
+
+     
