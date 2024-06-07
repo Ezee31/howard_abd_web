a12b1a2273e54930bac3e6f3d5b2ff6481884a20
Hector Montoya <hectormontoyadeveloper@gmail.com>
2024-06-07 12:06:08 -0600

    animacion de opcion seleccionada en el menu

--- howard_web/static/dashboard.js
+++ howard_web/static/dashboard.js
@@ -1,3 +1,42 @@
+(function () {
+  const currentPath = window.location.pathname;
+
+  switch (currentPath) {
+    case '/dashboard':
+      const elementoRaiz = document.querySelector("#option-raiz");
+      elementoRaiz.classList.add("selected");
+      break;
+    case '/grupo':
+      const elementoGrupo = document.querySelector("#option-grupo");
+      elementoGrupo.classList.add("selected");
+      break;
+    case '/alumno':
+      const elementoAlumno = document.querySelector("#option-alumno");
+      elementoAlumno.classList.add("selected");
+      break;
+    case '/profesor':
+      const elementoProfesor = document.querySelector("#option-profesor");
+      elementoProfesor.classList.add("selected");
+      break;
+    case '/horario':
+      const elementoHorario = document.querySelector("#option-horario");
+      elementoHorario.classList.add("selected");
+      break;
+    case '/tipo_turno':
+      const elementoTipoTurno = document.querySelector("#option-tipo-turno");
+      elementoTipoTurno.classList.add("selected");
+      break;
+    case '/pago':
+      const elementoPago = document.querySelector("#option-pago");
+      elementoPago.classList.add("selected");
+      break;
+    case '/tipo_pago':
+      const elementoTipoPago = document.querySelector("#option-tipo-pago");
+      elementoTipoPago.classList.add("selected");
+      break;
+  }
+})();
+
 //Ejecutar función en el evento click
 document.getElementById("btn_open").addEventListener("click", open_close_menu);
 
@@ -7,67 +46,26 @@ var btn_open = document.getElementById("btn_open");
 var body = document.getElementById("body");
 
 //Evento para mostrar y ocultar menú
-    function open_close_menu(){
-        body.classList.toggle("body_move");
-        side_menu.classList.toggle("menu__side_move");
-    }
+function open_close_menu(){
+    body.classList.toggle("body_move");
+    side_menu.classList.toggle("menu__side_move");
+}
 
 //Si el ancho de la página es menor a 760px, ocultará el menú al recargar la página
-
 if (window.innerWidth < 760){
-
     body.classList.add("body_move");
     side_menu.classList.add("menu__side_move");
 }
 
 //Haciendo el menú responsive(adaptable)
-
-window.addEventListener("resize", function(){
-
+window.addEventListener("resize", function() {
     if (window.innerWidth > 760){
-
         body.classList.remove("body_move");
         side_menu.classList.remove("menu__side_move");
     }
 
     if (window.innerWidth < 760){
-
         body.classList.add("body_move");
         side_menu.classList.add("menu__side_move");
     }
-
 });
-
-document.addEventListener("DOMContentLoaded", () => {
-    // Obtener las opciones del menú
-    const options = document.querySelectorAll(".options__menu a");
-
-    // Recorrer cada opción y agregar un listener de evento de clic
-    options.forEach(option => {
-        option.addEventListener("click", function (event) {
-            event.preventDefault(); // Prevenir la acción predeterminada del enlace
-            
-            // Remover la clase "selected" de la opción previamente seleccionada
-            const prevSelected = document.querySelector(".options__menu .selected");
-            if (prevSelected) {
-                prevSelected.classList.remove("selected");
-            }
-
-            // Agregar la clase "selected" a la opción actualmente seleccionada
-            option.classList.add("selected");
-
-            // Mover la barra seleccionada a la opción actualmente seleccionada
-            // Ajustar la posición de la barra si existe
-            const selectedBar = document.querySelector(".selected-bar");
-            if (selectedBar) {
-                selectedBar.style.top = `${option.offsetTop}px`; // Ajustar la posición de la barra
-            }
-
-            // Redirigir a la URL del enlace después de aplicar la selección
-            window.location.href = option.href;
-        });
-    });
-});
-
-
-
--- howard_web/urls.py
+++ howard_web/urls.py
@@ -3,43 +3,43 @@ from . import views
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    # Authentication views
+  # Authentication views
   path("", views.signin, name="signin"),
   path("dashboard", views.dashboard, name="dashboard"),
-  
+
   # TipoTurno views
   path("tipo_turno", views.tipo_turno, name="tipo_turno"),
   path("tipo_turno/<int:id>", views.tipo_turno, name="tipo_turno_edit"),
   path("tipo_turno_delete/<int:id>", views.tipo_turno_delete, name="tipo_turno_delete"),
-  
+
   # Horario views
   path("horario", views.horario, name="horario"),
   path("horario/<int:id>", views.horario, name="horario_edit"),
   path("horario_delete/<int:id>", views.horario_delete, name="horario_delete"),
-  
+
   # Profesor views
   path("profesor", views.profesor, name="profesor"),
   path("profesor/<int:id>", views.profesor, name="profesor_edit"),
   path("profesor_delete/<int:id>", views.profesor_delete, name="profesor_delete"),
-  
-   # TipoPago views
-  path("tipo_pago", views.tipo_pago, name="tipo_pago"),  
-  path("tipo_pago/<int:id>", views.tipo_pago, name="tipo_pago_edit"), 
+
+  # TipoPago views
+  path("tipo_pago", views.tipo_pago, name="tipo_pago"),
+  path("tipo_pago/<int:id>", views.tipo_pago, name="tipo_pago_edit"),
   path("tipo_pago_delete/<int:id>", views.tipo_pago_delete, name="tipo_pago_delete"),
-  
+
   # Grupo views
-  path("grupo", views.grupo, name="grupo"),  
-  path("grupo/<int:id>", views.grupo, name="grupo_edit"), 
+  path("grupo", views.grupo, name="grupo"),
+  path("grupo/<int:id>", views.grupo, name="grupo_edit"),
   path("grupo_delete/<int:id>", views.grupo_delete, name="grupo_delete"),
-  
+
   # Alumno views
-  path("alumno", views.alumno, name="alumno"),  
-  path("alumno/<int:id>", views.alumno, name="alumno_edit"), 
+  path("alumno", views.alumno, name="alumno"),
+  path("alumno/<int:id>", views.alumno, name="alumno_edit"),
   path("alumno_delete/<int:id>", views.alumno_delete, name="alumno_delete"),
-  
-  # Pago views 
-  path("pago", views.pago, name="pago"),     
-  path("pago/<int:id>", views.pago, name="pago_edit"),    
+
+  # Pago views
+  path("pago", views.pago, name="pago"),
+  path("pago/<int:id>", views.pago, name="pago_edit"),
   path("pago_delete/<int:id>", views.pago_delete, name="pago_delete"),
 
   # Logout view
